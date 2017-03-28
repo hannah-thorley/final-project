e6f6b1218d1e27c7a5adace406c759c2bbc92ee7
hannah-thorley <hannahthorley@hannah-thorleys-macbook-7.local>
2017-03-28 17:59:36 +0100

    Commit-3

--- js/main.js
+++ js/main.js
@@ -4,9 +4,51 @@ $(document).ready(function() {
   $('#prices-button').on('click', function(event){
 		event.preventDefault();
     $('#show-this-on-click').slideToggle();
-});
+  });
+  $('.readmore').on('click', function(event){
+		event.preventDefault();
+    $('#show-this-to-read').slideDown();
+    $('.readmore').hide();
+    $('.readless').show();
+  });
+  $('.readless').on('click', function(event){
+		event.preventDefault();
+	$('#show-this-to-read').slideUp();
+    $('.readless').hide();
+    $('.readmore').show();
+  });
+
+    $('.readmore-2').on('click', function(event){
+		event.preventDefault();
+    $('#show-this-to-read-2').slideDown();
+    $('.readmore-2').hide();
+    $('.readless-2').show();
+  });
+  $('.readless-2').on('click', function(event){
+		event.preventDefault();
+	$('#show-this-to-read-2').slideUp();
+    $('.readless-2').hide();
+    $('.readmore-2').show();
+  });
+
+  $('#about').mouseenter(function(){
+  	$('#about').fadeTo('fast', 1);
+  });
+
+    $('#session-info').mouseenter(function(){
+  	$('#session-info').fadeTo('fast', 1);
+  });
+
+    $('#testimonials').mouseenter(function(){
+  	$('#testimonials').fadeTo('fast', 1);
+  });
+
+    $('#contact').mouseenter(function(){
+  	$('#contact').fadeTo('fast', 1);
+  });
 
 $(function(){
-	$('.hero-carousel').slick();
+	$('.hero-carousel').slick()
 });
+
 });
